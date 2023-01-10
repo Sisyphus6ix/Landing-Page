563bc6a5cd50c1fd7d2aed6094e2d6128d850863
Ryan Silvera <ryan.silvera@icloud.com>
2023-01-10 00:21:47 -0500

    Finished home page

--- src/home.js
+++ src/home.js
@@ -1,28 +1,61 @@
 const pageHeader = () => {
     const content = document.getElementById('content')
-    
     const header = document.createElement('div')
     header.setAttribute('class', 'header')
     content.appendChild(header)
     
-    
     const title = document.createElement('h1')
-    title.innerText = 'This the page title'
+    title.innerText = 'Ryangweenee'
+    title.setAttribute('class', 'title')
     header.appendChild(title)
     
+    const navLinks = document.createElement('div')
+    navLinks.setAttribute('class', 'links')
+    header.appendChild(navLinks)
     const link1 = document.createElement('a')
     link1.setAttribute('href', '#')
-    link1.innerText = 'link1'
-    
+    link1.innerText = 'Home'
     const link2 = document.createElement('a')
     link2.setAttribute('href', '#')
-    link2.innerText = 'link2'
-    
+    link2.innerText = 'Menu'
     const link3 = document.createElement('a')
     link3.setAttribute('href', '#')
-    link3.innerText = 'link3'
+    link3.innerText = 'About'
     
-    header.appendChild(link1)
-    header.appendChild(link2)
-    header.appendChild(link3)
+    navLinks.appendChild(link1)
+    navLinks.appendChild(link2)
+    navLinks.appendChild(link3)
 }
+
+const homePageContent = () => {
+    const main = document.createElement('div')
+    main.setAttribute('class', 'main')
+    const message = document.createElement('p')
+    message.setAttribute('class', 'message')
+    message.innerText = 'Your local fan favorite!'
+    const image = document.createElement('img')
+    image.src = 'https://www.idlememe.com/wp-content/uploads/2021/10/salt-bae-meme-idlememe.jpg'
+    const message2 = document.createElement('p')
+    message2.setAttribute('class', 'message')
+    message2.innerText = 'Serving you since 1890'
+
+    content.appendChild(main) 
+    main.appendChild(message)
+    main.appendChild(image)
+    main.appendChild(message2)
+}
+
+const pageFooter = () => {
+    const footer = document.createElement('div')
+    footer.setAttribute('class', 'footer')
+    const footerInfo = document.createElement('p')
+    footerInfo.innerText = 'Copyright © 2023 Sisyphus6ix'
+    
+    content.appendChild(footer)
+    footer.appendChild(footerInfo)
+}
+
+
+export { pageHeader }
+export { homePageContent }
+export { pageFooter }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import { pageHeader } from "/src/home.js";
+import { homePageContent } from "/src/home.js";
+import { pageFooter } from "/src/home.js";
 
 pageHeader()
+homePageContent()
+pageFooter()
